ea8ba85a74e843e27352d1943f38a907ba5fe6b5
krishshah8000 <krishshah8000@gmail.com>
2026-03-18 22:10:37 +0530

    Time: 231 ms (10.30%) | Memory: 9.8 MB (98.22%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isFreqSame(int freq[],int winFreq[]){
+        for(int i=0;i<26;i++){
+            if(freq[i]!=winFreq[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        int freq[26]={0};
+
+        for(int i=0;i<s1.length();i++){
+            freq[s1[i]-'a']++;
+        }
+
+        for(int i=0;i<s2.length();i++){
+            int winIdx=0,idx=i;
+            int winFreq[26]={0};
+
+            while(winIdx<s1.length() && idx<s2.length()){
+                winFreq[s2[idx]-'a']++;
+                winIdx++;idx++;
+            }
+
+            if(isFreqSame(freq,winFreq)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
